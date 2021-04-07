bfbb38cc2f87aa944014c7f7ff438048c82d7aca
simasnau <simasnau@gmail.com>
2021-04-07 13:34:36 +0300

    Changed formatting of the file and added locks for thread safety

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import pylab as plt
 import threading
 
 def elementExistsByClass(name):
-    try: driver.find_element_by_class_name(name)
+    try:
+        driver.find_element_by_class_name(name)
     except NoSuchElementException:
         print("Element not found")
         return False
@@ -21,32 +22,42 @@ def getData():
         while elementExistsByClass('next'):
 
             # Gets all posts in a page.
-            posts=driver.find_elements_by_class_name('announcement-parameters')
+            posts = driver.find_elements_by_class_name(
+                'announcement-parameters')
 
             for post in posts:
-                stats=post.find_elements_by_tag_name('span')
-                metai=stats[0].text
-                if len(metai)>5: data=datetime.strptime(metai, '%Y-%m')  # Gets date depending on its format.
-                else: data=datetime.strptime(metai, '%Y')
+                stats = post.find_elements_by_tag_name('span')
+                metai = stats[0].text
+                if len(metai) > 5:
+                    # Gets date depending on its format.
+                    data = datetime.strptime(metai, '%Y-%m')
+                else:
+                    data = datetime.strptime(metai, '%Y')
 
-                if len(stats)>3 and 'km' in stats[3].text: # Gets mileage, it may be in different indexes in stats array.
-                    rida=int((stats[3].text.strip('km')).replace(" ",""))
-                    datos.append(data)
-                    ridos.append(rida)
-                elif len(stats)>4 and 'km' in stats[4].text:
-                    rida=int((stats[4].text.strip('km')).replace(" ",""))
-                    datos.append(data)
-                    ridos.append(rida)
-                else: rida=0
-                print(metai,rida)
+                with lock:
+                    # Gets mileage, it may be in different indexes in stats array.
+                    if len(stats) > 3 and 'km' in stats[3].text:
+                        rida = int(
+                            (stats[3].text.strip('km')).replace(" ", ""))
+                        datos.append(data)
+
+                        ridos.append(rida)
+                    elif len(stats) > 4 and 'km' in stats[4].text:
+                        rida = int(
+                            (stats[4].text.strip('km')).replace(" ", ""))
+                        datos.append(data)
+
+                        ridos.append(rida)
+                    else:
+                        rida = 0
+                print(data, rida)
 
             # Finds "next" button and presses it
-            nextBut=driver.find_element_by_class_name('next')
-            driver.execute_script('arguments[0].click()',nextBut)
-            
+            nextBut = driver.find_element_by_class_name('next')
+            driver.execute_script('arguments[0].click()', nextBut)
+
             # Sleeps to not get kicked out.
             sleep(0.5)
-
 
     except Exception as e:
         print(e)
@@ -56,21 +67,25 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://autoplius.lt/skelbimai/naudoti-automobiliai")
 sleep(0.2)
 
+lock = threading.Lock()
 try:
-    datos=[]
-    ridos=[]
+    datos = []
+    ridos = []
 
     # Creates a thread for getting car data and starts it
-    getDataThread=threading.Thread(target=getData,args=())
+    getDataThread = threading.Thread(target=getData, args=())
     getDataThread.start()
 
     # Main loop, draws data.
     while True:
         plt.clf()
-        plt.scatter(datos,ridos,s=2, color="blue")
+        with lock:
+            if len(datos) == len(ridos):
+                plt.scatter(datos, ridos, s=2, color="blue")
+
         plt.pause(0.05)
 
-        
+
 except KeyboardInterrupt:
     print("While was interrupted")
 
