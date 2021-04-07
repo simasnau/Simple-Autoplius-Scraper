4247f660b186579be0368214a3ca98dcfa8733f8
simasnau <simasnau@gmail.com>
2021-04-07 18:21:56 +0300

    Added info logging to csv file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from matplotlib import colors
+from matplotlib.pyplot import title
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
@@ -6,6 +7,9 @@ from datetime import datetime
 from selenium.common.exceptions import NoSuchElementException
 import pylab as plt
 import threading
+import csv
+import re
+
 
 def elementExistsByClass(name):
     try:
@@ -16,56 +20,68 @@ def elementExistsByClass(name):
     return True
 
 
-
 def getData():
-    try:
-        while elementExistsByClass('next'):
 
-            # Gets all posts in a page.
-            posts = driver.find_elements_by_class_name(
-                'announcement-parameters')
+    writer = csv.writer(csvFile)
+    writer.writerow(('Model', 'Price', 'Year',
+                    'Mileage', 'Displacement', 'Power', 'Link'))
 
-            for post in posts:
-                stats = post.find_elements_by_tag_name('span')
-                metai = stats[0].text
+    while elementExistsByClass('next'):
+
+        # Gets all posts in a page.
+        posts = driver.find_elements_by_class_name('announcement-item')
+
+        for post in posts:
+            try:
+                title=str(post.find_element_by_class_name('announcement-title').text)
+
+                displacement=title.split(",")[1].strip()
+                if re.match("^ ?[0-9]\.[0-9] l\.$", displacement):
+                    displacement=displacement[:-3]
+                else : displacement=0
+
+                makeModel=title.split(",")[0]
+                print("Title: ", makeModel)
+                params = post.find_element_by_class_name('bottom-aligner')
+                
+                link=post.get_attribute("href")
+                price=str(post.find_element_by_class_name('announcement-pricing-info').text).split("€")[0]
+                metai = params.find_element_by_xpath('.//span[@title="Pagaminimo data"]').text
+                galia = params.find_element_by_xpath('.//span[@title="Galia"]').text
+
                 if len(metai) > 5:
                     # Gets date depending on its format.
                     data = datetime.strptime(metai, '%Y-%m')
                 else:
                     data = datetime.strptime(metai, '%Y')
 
+                rida = int(params.find_element_by_xpath('.//span[@title="Rida"]').text.strip('km').replace(" ", ""))
+
                 with lock:
-                    # Gets mileage, it may be in different indexes in stats array.
-                    if len(stats) > 3 and 'km' in stats[3].text:
-                        rida = int(
-                            (stats[3].text.strip('km')).replace(" ", ""))
-                        datos.append(data)
+                    datos.append(data)
+                    ridos.append(rida)
 
-                        ridos.append(rida)
-                    elif len(stats) > 4 and 'km' in stats[4].text:
-                        rida = int(
-                            (stats[4].text.strip('km')).replace(" ", ""))
-                        datos.append(data)
+                print(price)
 
-                        ridos.append(rida)
-                    else:
-                        rida = 0
-                print(data, rida)
+                writer.writerow((makeModel, price, data, rida, displacement, galia, link))
+            except Exception as e:
+                print(e)
 
-            # Finds "next" button and presses it
-            nextBut = driver.find_element_by_class_name('next')
-            driver.execute_script('arguments[0].click()', nextBut)
+        # Finds "next" button and presses it
+        nextBut = driver.find_element_by_class_name('next')
+        driver.execute_script('arguments[0].click()', nextBut)
 
-            # Sleeps to not get kicked out.
-            sleep(0.5)
+        # Sleeps to not get kicked out.
+        sleep(0.5)
 
-    except Exception as e:
-        print(e)
 
 # WebDriver setup
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://autoplius.lt/skelbimai/naudoti-automobiliai")
 sleep(0.2)
+
+csvFile = open('autoData.csv', 'w+', newline='')
+
 
 lock = threading.Lock()
 try:
